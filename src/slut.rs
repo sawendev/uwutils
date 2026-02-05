9e0e40d15755ab1b70120f0c6b2d29eac458dc6e
sawendev <77589154+sawendev@users.noreply.github.com>
2026-02-04 19:53:18 -0600

    Move slut functions to their own struct

--- src/slut.rs
+++ src/slut.rs
@@ -1,56 +1,82 @@
 // SLice UTils - Helper functions for extracting data from byte slices
 
-pub fn take<T, const N: usize>(bytes: &mut &[u8], f: impl FnOnce([u8; N]) -> T) -> Option<T> {
-	take_arr(bytes).map(|a| f(a))
+pub struct Slut<'a> {
+	bytes: &'a [u8]
 }
 
-pub fn take_u8(bytes: &mut &[u8]) -> Option<u8> {
-	take(bytes, u8::from_le_bytes)
-}
-
-pub fn take_le_u16(bytes: &mut &[u8]) -> Option<u16> {
-	take(bytes, u16::from_le_bytes)
-}
-
-pub fn take_be_u16(bytes: &mut &[u8]) -> Option<u16> {
-	take(bytes, u16::from_be_bytes)
-}
-
-pub fn take_le_u32(bytes: &mut &[u8]) -> Option<u32> {
-	take(bytes, u32::from_le_bytes)
-}
-
-pub fn take_be_u32(bytes: &mut &[u8]) -> Option<u32> {
-	take(bytes, u32::from_be_bytes)
-}
-
-pub fn take_le_u64(bytes: &mut &[u8]) -> Option<u64> {
-	take(bytes, u64::from_le_bytes)
-}
-
-pub fn take_be_u64(bytes: &mut &[u8]) -> Option<u64> {
-	take(bytes, u64::from_be_bytes)
-}
-
-pub fn take_le_u128(bytes: &mut &[u8]) -> Option<u128> {
-	take(bytes, u128::from_le_bytes)
-}
-
-pub fn take_be_u128(bytes: &mut &[u8]) -> Option<u128> {
-	take(bytes, u128::from_be_bytes)
-}
-
-pub fn take_arr<const N: usize>(bytes: &mut &[u8]) -> Option<[u8; N]> {
-	take_slice(bytes, N).map(|s| s.try_into().unwrap())
-}
-
-pub fn take_slice<'a>(bytes: &mut &'a [u8], len: usize) -> Option<&'a [u8]> {
-	bytes.split_at_checked(len).map(|(l, r)| {
-		*bytes = r;
-		l
-	})
-}
-
-pub fn take_str<'a>(bytes: &mut &'a [u8], len: usize) -> Option<&'a str> {
-	take_slice(bytes, len).and_then(|s| str::from_utf8(s).ok())
+impl<'a> Slut<'a> {
+	pub fn new(src: &'a [u8]) -> Self {
+		Self { bytes: src }
+	}
+	
+	pub fn len(&self) -> usize {
+		self.bytes.len()
+	}
+	
+	pub fn take<T, const N: usize>(&mut self, f: impl FnOnce([u8; N]) -> T) -> Option<T> {
+		self.take_arr().map(|a| f(a))
+	}
+	
+	pub fn take_u8(&mut self) -> Option<u8> {
+		self.take(u8::from_le_bytes)
+	}
+	
+	pub fn take_le_u16(&mut self) -> Option<u16> {
+		self.take(u16::from_le_bytes)
+	}
+	
+	pub fn take_be_u16(&mut self) -> Option<u16> {
+		self.take(u16::from_be_bytes)
+	}
+	
+	pub fn take_le_u32(&mut self) -> Option<u32> {
+		self.take(u32::from_le_bytes)
+	}
+	
+	pub fn take_be_u32(&mut self) -> Option<u32> {
+		self.take(u32::from_be_bytes)
+	}
+	
+	pub fn take_le_u64(&mut self) -> Option<u64> {
+		self.take(u64::from_le_bytes)
+	}
+	
+	pub fn take_be_u64(&mut self) -> Option<u64> {
+		self.take(u64::from_be_bytes)
+	}
+	
+	pub fn take_le_u128(&mut self) -> Option<u128> {
+		self.take(u128::from_le_bytes)
+	}
+	
+	pub fn take_be_u128(&mut self) -> Option<u128> {
+		self.take(u128::from_be_bytes)
+	}
+	
+	pub fn take_arr<const N: usize>(&mut self) -> Option<[u8; N]> {
+		self.take_slice(N).map(|s| s.try_into().unwrap())
+	}
+	
+	pub fn take_slice(&mut self, len: usize) -> Option<&'a [u8]> {
+		self.bytes.split_at_checked(len).map(|(l, r)| { self.bytes = r; l })
+	}
+	
+	pub fn take_str(&mut self, len: usize) -> Option<&'a str> {
+		self.bytes.split_at_checked(len).and_then(|(l, r)| {
+			str::from_utf8(l).map(|s| { self.bytes = r; s }).ok()
+		})
+	}
+	
+	pub fn take_valid_utf8(&mut self) -> &'a str {
+		let s = match str::from_utf8(self.bytes) {
+			Ok(s) => s,
+			Err(e) => {
+				let len = e.valid_up_to();
+				// I FEAR NO GOD; GOD FEARS ME.
+				unsafe { str::from_utf8_unchecked(&self.bytes[..len]) }
+			}
+		};
+		self.bytes = &self.bytes[s.len()..];
+		s
+	}
 }
