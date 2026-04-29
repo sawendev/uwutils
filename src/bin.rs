709a7ec73dc8c9cb99cd1208729f3ae533126d48
Sawen <77589154+sawendev@users.noreply.github.com>
2026-04-29 09:15:08 -0500

    Add take functions for ints and floats

--- src/bin.rs
+++ src/bin.rs
@@ -10,6 +10,29 @@ impl<'a> BinReader<'a> {
 		Self { src, idx: 0 }
 	}
 	
+	pub fn take_u8(&mut self) -> Option<u8> { self.take(u8::from_le_bytes) }
+	pub fn take_i8(&mut self) -> Option<i8> { self.take(i8::from_le_bytes) }
+	pub fn take_le_u16(&mut self) -> Option<u16> { self.take(u16::from_le_bytes) }
+	pub fn take_le_i16(&mut self) -> Option<i16> { self.take(i16::from_le_bytes) }
+	pub fn take_be_u16(&mut self) -> Option<u16> { self.take(u16::from_be_bytes) }
+	pub fn take_be_i16(&mut self) -> Option<i16> { self.take(i16::from_be_bytes) }
+	pub fn take_le_u32(&mut self) -> Option<u32> { self.take(u32::from_le_bytes) }
+	pub fn take_le_i32(&mut self) -> Option<i32> { self.take(i32::from_le_bytes) }
+	pub fn take_be_u32(&mut self) -> Option<u32> { self.take(u32::from_be_bytes) }
+	pub fn take_be_i32(&mut self) -> Option<i32> { self.take(i32::from_be_bytes) }
+	pub fn take_le_u64(&mut self) -> Option<u64> { self.take(u64::from_le_bytes) }
+	pub fn take_le_i64(&mut self) -> Option<i64> { self.take(i64::from_le_bytes) }
+	pub fn take_be_u64(&mut self) -> Option<u64> { self.take(u64::from_be_bytes) }
+	pub fn take_be_i64(&mut self) -> Option<i64> { self.take(i64::from_be_bytes) }
+	pub fn take_le_u128(&mut self) -> Option<u128> { self.take(u128::from_le_bytes) }
+	pub fn take_le_i128(&mut self) -> Option<i128> { self.take(i128::from_le_bytes) }
+	pub fn take_be_u128(&mut self) -> Option<u128> { self.take(u128::from_be_bytes) }
+	pub fn take_be_i128(&mut self) -> Option<i128> { self.take(i128::from_be_bytes) }
+	pub fn take_le_f32(&mut self) -> Option<f32> { self.take(f32::from_le_bytes) }
+	pub fn take_be_f32(&mut self) -> Option<f32> { self.take(f32::from_be_bytes) }
+	pub fn take_le_f64(&mut self) -> Option<f64> { self.take(f64::from_le_bytes) }
+	pub fn take_be_f64(&mut self) -> Option<f64> { self.take(f64::from_be_bytes) }
+	
 	pub fn take<const LEN: usize, T>(&mut self, f: fn([u8; LEN]) -> T) -> Option<T> {
 		self.take_arr().map(|a| f(a))
 	}
